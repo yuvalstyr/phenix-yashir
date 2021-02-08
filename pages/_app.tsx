dc460ff54f98e940c878bb01dfe0637060cab310
yuval Styr <yuval.styr@hp.com>
2021-02-08 10:34:43 +0200

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "theme-ui";
 import theme from "../theme";
-import FacebookPixel from "../components/FacebookPixel";
 
 function MyApp({ Component, pageProps }) {
   return (
